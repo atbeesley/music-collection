681e766f4e5735f146044f93570a270ca296ee1a
user <you@exampalex.com>
2019-09-25 15:42:02 +0100

    MVP completed

--- models/artist.rb
+++ models/artist.rb
@@ -20,6 +20,16 @@ class Artist
     @id = SqlRunner.run(sql, values)[0]["id"].to_i()
   end
 
+  def albums()
+
+    sql = "SELECT * FROM albums WHERE artist_id = $1"
+    values = [@id]
+    results = SqlRunner.run(sql, values)
+    albums = results.map{|album_hash| Album.new(album_hash)}
+    return albums
+
+  end
+
   def self.all()
     sql = "SELECT * FROM artists"
     results = SqlRunner.run(sql)
