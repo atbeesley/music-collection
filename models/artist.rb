a73a97d51dfe36b6b5b2c45c0cba0438e07ac4b1
user <you@exampalex.com>
2019-09-25 14:48:19 +0100

    sql runner and artist save function implemented

---
+++ models/artist.rb
@@ -0,0 +1,23 @@
+require('pg')
+reqires_relative('../db/sql_runner')
+require_relative('album')
+
+class Artist
+
+  attr_accessor :name
+  attr_reader :id
+
+  def initialize( options )
+    @id = options['id'].to_i() if options['id']
+    @name = options['name']
+  end
+
+  def save()
+    sql = "INSERT INTO artists (name)
+    VALUES ($1)
+    RETURNING id"
+    values = [@name]
+    @id = SqlRunner.run(sql, values)[0]["id"].to_i()
+  end
+
+end
