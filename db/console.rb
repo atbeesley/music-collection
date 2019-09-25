4110a3f3f88b1fa4151160bf29d13c44ac38baff
user <you@exampalex.com>
2019-09-25 15:34:39 +0100

    album.artist method created and tested

--- db/console.rb
+++ db/console.rb
@@ -1,9 +1,16 @@
 require("pry")
 require_relative('../models/artist')
+require_relative('../models/album')
 
 artist1 = Artist.new({'name' => 'The Beatles'})
 
 artist1.save
+
+album1 = Album.new({'title' => 'Abbey Road', 'genre' => 'Rock/Pop', 'artist_id' => artist1.id })
+album2 = Album.new({'title' => 'Rubber Soul', 'genre' => 'Rock/Pop', 'artist_id' => artist1.id })
+
+album1.save
+album2.save
 
 binding.pry
 nil
---
+++ models/album.rb
@@ -0,0 +1,42 @@
+require('pg')
+require_relative('../db/sql_runner')
+require_relative('artist')
+
+class Album
+
+  attr_accessor :title, :genre
+  attr_reader :id, :artist_id
+
+  def initialize( options )
+    @id = options['id'].to_i() if options['id']
+    @title = options['title']
+    @genre = options['genre']
+    @artist_id = options['artist_id']
+  end
+
+  def save()
+    sql = "INSERT INTO albums (title, genre, artist_id)
+    VALUES ($1, $2, $3)
+    RETURNING id"
+    values = [@title, @genre, @artist_id]
+    @id = SqlRunner.run(sql, values)[0]["id"].to_i()
+  end
+
+  def artist()
+
+    sql = "SELECT * FROM artists WHERE id = $1"
+    values = [@artist_id]
+    results = SqlRunner.run(sql, values)
+    artist_hash = results[0]
+    artist = Artist.new(artist_hash)
+    return artist
+    
+  end
+
+  def self.all()
+    sql = "SELECT * FROM albums"
+    results = SqlRunner.run(sql)
+    return results.map { |album| Album.new(album) }
+  end
+
+end
